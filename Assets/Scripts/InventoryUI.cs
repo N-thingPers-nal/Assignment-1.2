2f03c9d4a86a431c29722192c996352631c6148c
N-thingPers-nal <145038064+N-thingPers-nal@users.noreply.github.com>
2024-01-09 16:53:09 +0000

    Version 1.2
    Added:
    - collectables
    - level select menu

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InventoryUI : MonoBehaviour
+{
+    private TextMeshProUGUI diamondText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        diamondText = GetComponent<TextMeshProUGUI>();
+    }
+
+   public void UpdateDiamondText(PlayerInventory playerInventory)
+    {
+        diamondText.text = playerInventory.NumberOfDiamonds.ToString();
+    }
+
+    /*
+    public void UpdateDiamondUIText(int numberOfDiamonds)
+    {
+        diamondText.text = numberOfDiamonds.ToString();
+    }
+    */
+}
