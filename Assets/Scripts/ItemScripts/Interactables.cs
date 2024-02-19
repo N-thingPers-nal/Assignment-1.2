356fd51d1b61d9a085b67442da167c2ee4729d32
N-thingPers-nal <145038064+N-thingPers-nal@users.noreply.github.com>
2024-02-19 23:53:24 +0000

    Updates to lobby
    - Lobby improvements
    - level select (needs to be updated, not
     done in the best way)

---
+++ Assets/Scripts/ItemScripts/Interactables.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+interface IInteractable
+{
+    public void Interact();
+}
+
+
+public class Interactables : MonoBehaviour
+{
+    //public Transform InteractorSource;
+    public float InteractRange;
+    public Transform InteractionTransform;
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact() // enables different interactable outcomes for each type
+    {
+        Debug.Log("INTERACTING WITH " + transform.name);
+    }
+
+    private void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, InteractionTransform.position);  //check distance to player
+            if (distance <= InteractRange) 
+            {
+                Debug.Log("INTERACT");
+                Interact();
+                hasInteracted = true; //only interact once per frame
+            }
+        }
+    }
+
+    public void OnFocused (Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false; // only interact once once focused
+    }
+
+    public void OnDefocused ()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (InteractionTransform == null)
+            InteractionTransform = transform;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(InteractionTransform.position, InteractRange);
+    }
+
+}
